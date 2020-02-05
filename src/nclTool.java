3a68e828680e66b4e3e79c27087daecff46142c4
ohneyan <jorgesuarez2112@gmail.com>
2020-02-04 21:27:41 -0500

    Completed Script

--- src/nclTool.java
+++ src/nclTool.java
@@ -1,27 +1,23 @@
+/*
+ * Written By: Jorge Suarez | Github: ohneyan
+ * Scenario Selected: 1
+ * Scenario Description: Guest explores Ports of Departure
+ * */
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class nclTool
 {
-
 	public static void main(String[] args)
 	{
 	
-		//Open the Chrome Browser Driver and creating the Driver object
+		//Open the Chrome Browser Driver and create the Driver object
 		System.setProperty("webdriver.chrome.driver", "C:\\IDEs & Frameworks\\Selenium\\ChromeDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
-		/*DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--incognito");
-		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-		WebDriver driver = new ChromeDriver(capabilities);
-		*/
-		
-		//Boolean for Image
+			
+		//Boolean for Image validation
 		Boolean imgPresent = driver.findElements(By.xpath("//*[@id=\'IPEinvL850\']/img")).size() > 0;
 		
 		//Open Chrome driver for NCL.com
@@ -33,15 +29,11 @@ public class nclTool
 			driver.findElement(By.xpath("//*[@id=\'IPEinvL850\']/map/area[1]")).click();
 		}
 
-		
-		
-		//This will click on the ports page 
+		//This will click on the ports link 
 		By.className("linkItem");
 		driver.findElement(By.linkText("Ports")).click();
 		
-		//Check for popup image
-		
-		//sleep for page load
+		//sleep to allow page to load
 		try
 		{
 			Thread.sleep(11000);
@@ -53,7 +45,6 @@ public class nclTool
 		//checks for popup image
 		if(imgPresent == true)
 		{
-			//This works - I just need to find a if statement to verify if its there
 			driver.findElement(By.xpath("//*[@id=\'IPEinvL850\']/map/area[1]")).click();
 		}
 		
@@ -63,5 +54,4 @@ public class nclTool
 		//If there is a port present after entering port name, click it
 		driver.findElement(By.xpath("//*[@id=\'searchArea\']/div[2]/ul/li[2]/a")).click();
 	}
-
 }
